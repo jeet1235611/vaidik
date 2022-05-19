dd617d65da3170fa6f2103e9970eeb2748ecf12d
acoustic_jeet <js.jitendra11@gmail.com>
2022-05-19 10:10:38 +0530

    Create DefaultConstructorCheck.cpp

---
+++ DefaultConstructorCheck.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class test {
+    public:
+    test() {
+        cout << "Hello World!" << endl;
+    }    
+};
+
+int main() {
+        test t;
+       // cout << t.test() << endl;
+        
+}
+
+
+Output:
+Hellow World
